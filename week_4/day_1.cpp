96328e5b88fa13f7db6e1caf0b66dd493204762c
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-06-22 16:31:51 +0530

    Implement binary tree algorithms: maxDepth, invertTree, isSameTree

--- week_4/day_1.cpp
+++ week_4/day_1.cpp
@@ -1,1 +1,129 @@
 // day_1.cpp
+
+104. Maximum Depth of Binary Tree
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+         if (root == nullptr) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+
+
+
+-----------------------------------------------------------------------------------
+
+
+
+  226. Invert Binary Tree
+
+
+
+
+  /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+       
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+      
+        TreeNode* temp = root->right;
+        root->right = root->left;
+        root->left = temp;
+
+       
+        invertTree(root->left);
+        invertTree(root->right);
+
+       
+        return root;
+    }
+};
+
+
+
+
+
+----------------------------------------------------------
+
+
+
+
+  100. Same Tree
+
+
+
+
+
+  /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* r1, TreeNode* r2) {
+         if(r1 == NULL && r2 == NULL) {
+            return true;
+        }
+        
+        if(r1 == NULL && r2 != NULL) {
+            return false;
+        }
+        
+        if(r1 != NULL && r2 == NULL) {
+            return false;
+        }
+        
+        bool left = isSameTree(r1->left, r2->left);
+        bool right = isSameTree(r1->right, r2->right);
+        
+        bool value = r1->val == r2->val;
+        
+        
+        if(left && right && value) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    
+        
+    }
+};
